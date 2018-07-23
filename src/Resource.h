f192aa2fe9eddd0b73801539b90267091da4402e
dormon <imilet@fit.vutbr.cz>
2018-07-23 19:34:38 +0200

    Reformated Resource.h

--- src/Resource.h
+++ src/Resource.h
@@ -1,27 +1,27 @@
 #pragma once
 
-#include<functional>
+#include <functional>
 
-namespace vars{
-  class Resource;
-  using Destructor = std::function<void(void*)>;
-  using OnChange   = std::function<void()>     ;
-};
+namespace vars {
+class Resource;
+using Destructor = std::function<void(void*)>;
+using OnChange   = std::function<void()>;
+};  // namespace vars
 
-class vars::Resource{
+class vars::Resource {
  public:
   Resource(void* d, Destructor const& dst, std::type_info const& t);
   ~Resource();
-  void updateTicks();
-  size_t getTicks()const;
-  std::type_info const& getType()const;
-  void setChangeCallback(OnChange const&clb);
-  void* getData()const;
+  void                  updateTicks();
+  size_t                getTicks() const;
+  std::type_info const& getType() const;
+  void                  setChangeCallback(OnChange const& clb);
+  void*                 getData() const;
+
  private:
   void*                 data;
-  size_t                ticks = 1;
+  size_t                ticks          = 1;
   OnChange              changeCallback = nullptr;
   Destructor            destructor;
   std::type_info const& type;
 };
-
