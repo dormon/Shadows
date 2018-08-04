e8597a1cb3e76cecfbe186b98b87f8ddb715027a
dormon <imilet@fit.vutbr.cz>
2018-08-04 20:04:57 +0200

    Refactored DrawCaps

--- src/CSSV/DrawCaps.cpp
+++ src/CSSV/DrawCaps.cpp
@@ -15,23 +15,34 @@ shared_ptr<VertexArray>createCapsVao(shared_ptr<Buffer>const&caps){
   return vao;
 }
 
+void copyVertex(float*const dst,float const*const src){
+  size_t const sizeofVertex3DInBytes = componentsPerVertex3D * sizeof(float);
+  memcpy(dst,src,sizeofVertex3DInBytes);
+  dst[3] = 1.f;
+}
+
+void copyTriangle(float*const dst,float const*const src){
+  for(size_t p=0;p<verticesPerTriangle;++p){
+    auto   const vertexDstPtr = dst + p*componentsPerVertex4D;
+    auto   const vertexSrcPtr = src + p*componentsPerVertex3D;
+    copyVertex(vertexDstPtr,vertexSrcPtr);
+  }
+}
+
+void copyTriangles(float*const dst,float const*const src,size_t nofTriangles){
+  for(size_t t=0;t<nofTriangles;++t){
+    auto const triangleDstPtr = dst + t*componentsPerVertex4D*verticesPerTriangle;
+    auto const triangleSrcPtr = src + t*componentsPerVertex3D*verticesPerTriangle;
+    copyTriangle(triangleDstPtr,triangleSrcPtr);
+  }
+}
+
 shared_ptr<Buffer>createCapsBuffer(shared_ptr<Adjacency const>const&adj){
   auto const nofTriangles = adj->getNofTriangles();
   vector<float>dst(componentsPerVertex4D*verticesPerTriangle*nofTriangles);
   auto const dstPtr = dst.data();
   auto const srcPtr = adj->getVertices().data();
-  for(size_t t=0;t<nofTriangles;++t){
-    auto const triangleDstPtr = dstPtr + t*componentsPerVertex4D*verticesPerTriangle;
-    auto const triangleSrcPtr = srcPtr + t*componentsPerVertex3D*verticesPerTriangle;
-    for(size_t p=0;p<verticesPerTriangle;++p){
-      auto   const vertexDstPtr = triangleDstPtr + p*componentsPerVertex4D;
-      auto   const vertexSrcPtr = triangleSrcPtr + p*componentsPerVertex3D;
-      size_t const sizeofVertex3DInBytes = componentsPerVertex3D * sizeof(float);
-      memcpy(vertexDstPtr,vertexSrcPtr,sizeofVertex3DInBytes);
-      vertexDstPtr[3] = 1.f;
-    }
-  }
-  
+  copyTriangles(dstPtr,srcPtr,nofTriangles);
   return make_shared<Buffer>(dst);
 }
 
