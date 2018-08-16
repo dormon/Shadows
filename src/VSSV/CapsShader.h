d63a87d36c944b50da25f3dd8eb014c078a12d77
dormon <imilet@fit.vutbr.cz>
2018-08-16 15:10:19 +0200

    Refactored VSSV/CapsShader

--- src/VSSV/CapsShader.h
+++ src/VSSV/CapsShader.h
@@ -4,40 +4,30 @@
 
 std::string const vertexShaderSrc = R".(
 #line 6
-layout(location=0)in vec3 triangleVertexA;
-layout(location=1)in vec3 triangleVertexB;
-layout(location=2)in vec3 triangleVertexC;
+layout(location=0)in vec3 vertexA;
+layout(location=1)in vec3 vertexB;
+layout(location=2)in vec3 vertexC;
 
-uniform vec4 lightPosition    = vec4(10,10,10,1);
-uniform mat4 modelMatrix      = mat4(1.f);
-uniform mat4 viewMatrix       = mat4(1.f);
-uniform mat4 projectionMatrix = mat4(1.f);
+uniform vec4 light = vec4(10,10,10,1);
+uniform mat4 mvp   = mat4(1.f);
 
 void main(){
-  int multiplicity = currentMultiplicity(triangleVertexA,triangleVertexB,triangleVertexC,lightPosition);
+  int multiplicity = currentMultiplicity(vertexA,vertexB,vertexC,light);
+
   if(multiplicity==0){gl_Position=vec4(0,0,0,1);return;}
-  if(multiplicity>0){ // this has to be swapped if using strips
-    float farCap = float(gl_InstanceID&1);
-    int   vID    = ((farCap>0)?2-int(gl_VertexID):int(gl_VertexID));
 
-    vec4 P=vec4(triangleVertexB*float(vID==0)+triangleVertexA*float(vID==1)+triangleVertexC*float(vID==2),1.0);
-  
-    P=P*(1.0-farCap)+vec4(P.xyz*lightPosition.w-lightPosition.xyz,0)*farCap;
-    P=projectionMatrix*viewMatrix*modelMatrix*P;
-    P=P.xyww*(1.0-farCap)+P*farCap;//front cap
-    gl_Position=P;
+  float farCap = float(gl_InstanceID&1);
+  uint  vID    = ((farCap>0)?2-int(gl_VertexID):int(gl_VertexID));
+  vec4  P;
 
-  }else{
+  // this has to be swapped if using strips
+  vID ^= uint((multiplicity>0) && (vID<2));//swap 0 1
 
-    float farCap = float(gl_InstanceID&1);
-    int   vID    = ((farCap>0)?2-int(gl_VertexID):int(gl_VertexID));
+  P=vec4(vertexA*float(vID==0)+vertexB*float(vID==1)+vertexC*float(vID==2),1.0);
 
-    vec4 P=vec4(triangleVertexA*float(vID==0)+triangleVertexB*float(vID==1)+triangleVertexC*float(vID==2),1.0);
-  
-    P=P*(1.0-farCap)+vec4(P.xyz*lightPosition.w-lightPosition.xyz,0)*farCap;
-    P=projectionMatrix*viewMatrix*modelMatrix*P;
-    P=P.xyww*(1.0-farCap)+P*farCap;//front cap
-    gl_Position=P;
-  }
+  P = P*(1.0-farCap)+vec4(P.xyz*light.w-light.xyz,0)*farCap;
+  P = mvp*P;
+  P = P.xyww*(1.0-farCap)+P*farCap;//front cap
+  gl_Position = P;
 }
 ).";
--- src/VSSV/DrawCaps.cpp
+++ src/VSSV/DrawCaps.cpp
@@ -6,17 +6,17 @@
 
 using namespace ge::gl;
 using namespace std;
+using namespace glm;
 
 shared_ptr<VertexArray>createVAO(shared_ptr<Buffer>const&caps){
   auto vao = make_shared<VertexArray>();
-  GLsizei const stride             = GLsizei(sizeof(Triangle3Df));
-  GLenum  const normalized         = GL_FALSE;
-  size_t  const nofCapsPerTriangle = 2;
-  GLuint  const divisor            = GLuint(nofCapsPerTriangle);
-  for(size_t i=0;i<3;++i){
-    GLintptr offset = sizeof(Vertex3Df) * i;
-    GLuint   index = GLuint(i);
-    vao->addAttrib(caps,index,3,GL_FLOAT,stride,offset,normalized,divisor);
+  auto   const stride             = static_cast<GLsizei>(sizeof(Triangle3Df));
+  GLenum const normalized         = GL_FALSE;
+  size_t const nofCapsPerTriangle = 2;
+  auto   const divisor            = static_cast<GLuint> (nofCapsPerTriangle);
+  for(GLuint i=0;i<3;++i){
+    GLintptr const offset = sizeof(Vertex3Df) * i;
+    vao->addAttrib(caps,i,3,GL_FLOAT,stride,offset,normalized,divisor);
   }
   return vao;
 }
@@ -40,17 +40,17 @@ DrawCaps::DrawCaps(shared_ptr<Adjacency const>const&adj){
 }
 
 void DrawCaps::draw(
-    glm::vec4 const&lightPosition   ,
-    glm::mat4 const&viewMatrix      ,
-    glm::mat4 const&projectionMatrix){
-  program->setMatrix4fv("viewMatrix"      ,glm::value_ptr(viewMatrix      ))
-         ->setMatrix4fv("projectionMatrix",glm::value_ptr(projectionMatrix))
-         ->set4fv      ("lightPosition"   ,glm::value_ptr(lightPosition   ))
+    vec4 const&lightPosition   ,
+    mat4 const&viewMatrix      ,
+    mat4 const&projectionMatrix){
+  auto const mvp = projectionMatrix * viewMatrix;
+  program->setMatrix4fv("mvp"  ,value_ptr(mvp          ))
+         ->set4fv      ("light",value_ptr(lightPosition))
          ->use();
   vao->bind();
   size_t  const nofCapsPerTriangle = 2;
-  GLuint  const nofInstances = GLuint(nofCapsPerTriangle * nofTriangles);
-  GLsizei const nofVertices  = GLsizei(3);
+  auto    const nofInstances = static_cast<GLuint>(nofCapsPerTriangle * nofTriangles);
+  GLsizei const nofVertices  = 3;
   GLint   const firstVertex  = 0;
   glDrawArraysInstanced(GL_TRIANGLES,firstVertex,nofVertices,nofInstances);
   vao->unbind();
