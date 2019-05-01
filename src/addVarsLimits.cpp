52047af3c0b8342046718b1d100d844613ecd43b
Tomáš Milet <amillhouse@seznam.cz>
2019-05-01 03:50:37 +0200

    selectedPoint

--- src/addVarsLimits.cpp
+++ src/addVarsLimits.cpp
@@ -7,17 +7,13 @@ void setVarsLimitsPostfix(vars::Vars& vars,std::string const&name){
   vars.addString(limitsPostfixVariable,name);
 }
 
-
-
 void addVarsLimitsF(vars::Vars& vars,std::string const&name,float mmin,float mmax,float step){
   auto const postfix = vars.addOrGetString(limitsPostfixVariable,drawImguiVarsDefaultPostfix);
-  std::cerr << "name+postfix: " << (name+postfix) << std::endl;
-  vars.addOrGet<VarsLimits<float>>(name+postfix,mmin,mmax,step);
+  vars.reCreate<VarsLimits<float>>(name+postfix,mmin,mmax,step);
 }
 
 void addVarsLimitsU(vars::Vars& vars,std::string const&name,uint32_t mmin,uint32_t mmax,uint32_t step){
   auto const postfix = vars.addOrGetString(limitsPostfixVariable,drawImguiVarsDefaultPostfix);
-  std::cerr << "name+postfix: " << (name+postfix) << std::endl;
-  vars.addOrGet<VarsLimits<uint32_t>>(name+postfix,mmin,mmax,step);
+  vars.reCreate<VarsLimits<uint32_t>>(name+postfix,mmin,mmax,step);
 }
 
--- src/drawPointCloud.cpp
+++ src/drawPointCloud.cpp
@@ -18,16 +18,62 @@ void createPointCloudProgram(vars::Vars&vars){
   layout(binding=0)uniform  sampler2D positionTexture;
   layout(binding=1)uniform usampler2D    colorTexture;
   uniform mat4 mvp;
-  uniform uint selectedPoint = 0;
+  uniform uint selPoint   = 0;
+  uniform uint selLevel   = 0;
+  uniform  int drawColors = 0;
   out vec3 vColor;
+
+  #ifndef NOF_LEVELS
+  #define NOF_LEVELS 3
+  #endif//NOF_LEVELS
+
+
+  uniform uvec2 tileGlobalExponent[NOF_LEVELS] = {
+    uvec2(6),
+    uvec2(3),
+    uvec2(0),
+  };
+
+  uniform uvec2 tileGlobalMask[NOF_LEVELS] = {
+    uvec2(7),
+    uvec2(7),
+    uvec2(7),
+  };
+
+  uvec2 threadIdToCoord(uint id,uint levelWidth){
+    return uvec2(id % levelWidth , id / levelWidth);
+  }
+
+  uvec2 threadIdToTileCoord(uint id,uint levelWidth,uvec2 tileExponent,uvec2 tileMask){
+    uvec2 threadCoord = threadIdToCoord(id,levelWidth);
+    return (threadCoord>>tileExponent)&tileMask;
+  }
+
+  uvec2 toSnake(uvec2 coord,uvec2 size,uint yAxis){
+    uint odd = coord[yAxis];
+    uvec2 result;
+    result[1-yAxis] = coord[1-yAxis] + odd*uint(size[1-yAxis] - 2*coord[1-yAxis] - 1u);
+    result[  yAxis] = coord[  yAxis]                                                  ;
+    return result;
+  }
+
+
   void main(){
     uvec2 size     = textureSize(positionTexture,0);
-    ivec2 coord    = ivec2(gl_VertexID%size.x,gl_VertexID/size.x);
-    vec4  position = texelFetch(positionTexture,coord,0);
-    uvec4 color    = texelFetch(colorTexture   ,coord,0);
-    vColor         = vec3((color.xyz>>0u)&0xffu)/0xffu;
+    uvec2 coord    = ivec2(threadIdToCoord(gl_VertexID,size.x));
 
-    if(gl_VertexID == selectedPoint){
+
+    vec4  position = texelFetch(positionTexture,ivec2(coord),0);
+
+    if(drawColors == 1){
+      uvec4 color = texelFetch(colorTexture   ,ivec2(coord),0);
+      vColor      = vec3((color.xyz>>0u)&0xffu)/0xffu;
+    }else{
+      uvec2 tileCoord    = threadIdToTileCoord(gl_VertexID,size.x,tileGlobalExponent[selLevel],tileGlobalMask[selLevel]);
+      vColor = vec3((tileCoord.x&1u) ^ (tileCoord.y&1u));
+    }
+
+    if(gl_VertexID == selPoint){
       gl_PointSize = 10;
       vColor = vec3(1,0,0);
     }else
@@ -46,20 +92,18 @@ void createPointCloudProgram(vars::Vars&vars){
   }
   ).";
   auto const fs = make_shared<Shader>(GL_FRAGMENT_SHADER,fsSrc);
-  vars.add<Program>("drawPointCloudProgram",vs,fs);
+  vars.add<Program>("pointCloud.program",vs,fs);
 }
 
 void createPointCloudVAO(vars::Vars&vars){
   FUNCTION_PROLOGUE("all");
-  vars.add<VertexArray>("pointCloudVAO");
+  vars.add<VertexArray>("pointCloud.vao");
 }
 
 void drawPointCloud(vars::Vars&vars){
   FUNCTION_CALLER();
-  if(!vars.has("gBufferAsPointCloud"))return;
+  if(!vars.has("pointCloud.positionTexture"))return;
 
-  uint32_t selectedPoint = vars.addOrGetUint32("selectedPoint");
-  addVarsLimitsU(vars,"selectedPoint",0,100,10);
   createPointCloudProgram(vars);
   createPointCloudVAO(vars);
 
@@ -67,19 +111,41 @@ void drawPointCloud(vars::Vars&vars){
   glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
   glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
 
-  auto program         = vars.get<Program    >("drawPointCloudProgram"   );
-  auto vao             = vars.get<VertexArray>("pointCloudVAO"           );
-  auto window          = vars.get<uvec2      >("windowSize"              );
-  auto pointCloud      = vars.get<Texture    >("gBufferAsPointCloud"     );
-  auto pointCloudColor = vars.get<Texture    >("gBufferAsPointCloudColor");
+  auto window          = vars.get<uvec2      >("windowSize"                );
+  auto program         = vars.get<Program    >("pointCloud.program"        );
+  auto vao             = vars.get<VertexArray>("pointCloud.vao"            );
+  auto pointCloud      = vars.get<Texture    >("pointCloud.positionTexture");
+  auto pointCloudColor = vars.get<Texture    >("pointCloud.colorTexture"   );
   
+  uint32_t selPointX = vars.addOrGetUint32("pointCloud.selPointX");
+  uint32_t selPointY = vars.addOrGetUint32("pointCloud.selPointY");
+  uint32_t selPoint  = vars.addOrGetUint32("pointCloud.selPoint" );
+  uint32_t selLevel  = vars.addOrGetUint32("pointCloud.selLevel",2);
+  bool     usePointID     = vars.addOrGetBool  ("pointCloud.usePointID"     );
+  bool     drawColors     = vars.addOrGetBool  ("pointCloud.drawColors");
+
+  uint32_t width = pointCloud->getWidth(0);
+  uint32_t height = pointCloud->getHeight(0);
+  addVarsLimitsU(vars,"pointCloud.selPointX",0,width-1,1);
+  addVarsLimitsU(vars,"pointCloud.selPointY",0,height-1,1);
+  addVarsLimitsU(vars,"pointCloud.selPoint" ,0,height*width-1,1);
+  addVarsLimitsU(vars,"pointCloud.selLevel" ,0,2,1);
+
   auto mvp = getMVP(vars);
   vao->bind();
   pointCloud->bind(0);
   pointCloudColor->bind(1);
-  program->setMatrix4fv("mvp",value_ptr(mvp));
-  program->set1ui("selectedPoint",selectedPoint);
-  program->use();
-  glDrawArrays(GL_POINTS,0,window->x*window->y);
+  program
+    ->setMatrix4fv("mvp"       ,value_ptr(mvp))
+    ->set1ui      ("selLevel"  ,selLevel      )
+    ->set1i       ("drawColors",drawColors    )
+    ->use();
+  
+  if(usePointID)
+    program->set1ui      ("selPoint" ,selPoint );
+  else
+    program->set1ui      ("selPoint" ,selPointY * width + selPointX );
+
+  glDrawArrays(GL_POINTS,0,width*height);
   vao->unbind();
 }
