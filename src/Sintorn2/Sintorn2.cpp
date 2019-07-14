22458a919ef3d29a2f8653e8cb76967eaf929cf0
dormon <imilet@fit.vutbr.cz>
2019-07-14 15:46:59 +0200

    Sintorn2

--- src/Sintorn2/Sintorn2.cpp
+++ src/Sintorn2/Sintorn2.cpp
@@ -18,6 +18,61 @@ void allocateHierarchy(vars::Vars&vars){
 
 }
 
+void createProgram(vars::Vars&vars){
+  FUNCTION_PROLOGUE("sintorn2","windowSize","wavefrontSize");
+  auto const src = R".(
+  layout(local_size_x=16,local_size_y=16)in;
+
+  layout(binding=2)uniform sampler2DRect depthTexture;
+  uniform uvec2 windowSize = uvec2(512,512);
+
+  uniform float near = 0.01f;
+  uniform uint Sy = 512/8;
+  uniform float fovy = 90;
+
+  uint convertDepth(float depth){
+    return uint(log(-depth/near) / log(1+2*tan(fovy)/Sy));
+  }
+
+  uint morton(uvec3 cc){
+    uint res = 0;
+    res |= (cc.y& 1)<< 0;//0
+    res |= (cc.x& 1)<< 1;//1
+    res |= (cc.z& 1)<< 2;//2
+    res |= (cc.y& 2)<< 2;//3
+    res |= (cc.x& 2)<< 3;//4
+    res |= (cc.z& 2)<< 4;//5
+    res |= (cc.y& 4)<< 4;//6
+    res |= (cc.x& 4)<< 5;//7
+    res |= (cc.z& 4)<< 6;//8
+    res |= (cc.y& 8)<< 6;//9
+    res |= (cc.x& 8)<< 7;//10
+    res |= (cc.z& 8)<< 8;//11
+    res |= (cc.y&16)<< 8;//12
+    res |= (cc.x&16)<< 9;//13
+    res |= (cc.z&16)<<10;//14
+    res |= (cc.y&32)<<10;//15
+    res |= (cc.x&32)<<11;//16
+    res |= (cc.z&32)<<12;//17
+    res |= (cc.y&64)<<12;//18
+    res |= (cc.x&64)<<13;//19
+    res |= (cc.z&64)<<14;//20a
+  }
+
+  #define GID uvec2(gl_GlobalInvocationID.xy)
+  void main(){
+    if(any(greaterThanEqual(GID,windowSize)))return;
+
+    float depth = texelFetch(depthTexture,ivec2(GID),0).x*2-1;
+    uvec3 clusterCoord = uvec3(uvec2(GID/8) , convertDepth(depth));
+    
+  }
+
+
+  ).";
+
+}
+
 void Sintorn2::create(glm::vec4 const& lightPosition,
                       glm::mat4 const& viewMatrix,
                       glm::mat4 const& projectionMatrix)
