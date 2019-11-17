701c6e29047cdf038270176f2814e00a95fc9e00
dormon <imilet@fit.vutbr.cz>
2019-11-17 05:13:58 +0100

    Removed leftover files

--- src/CSSV/InterleavedPlanesExtractSilhouettes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include<CSSV/ExtractSilhouettes.h>
-
-class cssv::InterleavedPlanesExtractSilhouettes: public ExtractSilhouettes{
-  public:
-    InterleavedPlanesExtractSilhouettes(vars::Vars&vars);
-};
-
--- src/CSSV/PlanesExtractSilhouettes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include<CSSV/ExtractSilhouettes.h>
-
-class cssv::PlanesExtractSilhouettes: public ExtractSilhouettes{
-  public:
-    PlanesExtractSilhouettes(vars::Vars&vars);
-};
-
