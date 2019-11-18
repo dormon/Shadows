88e3390e0aba19e03262e8a03f94726ba6602805
Tomáš Milet <amillhouse@seznam.cz>
2019-11-18 17:57:19 +0100

    Moved CubeShadowMapping parameters from args.csm.* to csm.param.* and refactored CSM

--- src/CubeShadowMapping/CubeShadowMapping.cpp
+++ src/CubeShadowMapping/CubeShadowMapping.cpp
@@ -10,10 +10,10 @@ using namespace std;
 #include<FunctionPrologue.h>
 
 void createShadowMapTexture(vars::Vars&vars){
-  FUNCTION_PROLOGUE("csm","args.csm.resolution");
+  FUNCTION_PROLOGUE("csm.method","csm.param.resolution");
 
-  auto const resolution = vars.getUint32("args.csm.resolution");
-  auto shadowMap = vars.reCreate<Texture>("csm.shadowMap",GL_TEXTURE_CUBE_MAP,GL_DEPTH_COMPONENT24,1,resolution,resolution);
+  auto const resolution = vars.getUint32("csm.param.resolution");
+  auto shadowMap = vars.reCreate<Texture>("csm.method.shadowMap",GL_TEXTURE_CUBE_MAP,GL_DEPTH_COMPONENT24,1,resolution,resolution);
   shadowMap->texParameteri(GL_TEXTURE_MIN_FILTER  ,GL_NEAREST             );
   shadowMap->texParameteri(GL_TEXTURE_MAG_FILTER  ,GL_NEAREST             );
   shadowMap->texParameteri(GL_TEXTURE_WRAP_S      ,GL_CLAMP_TO_EDGE       );
@@ -23,25 +23,25 @@ void createShadowMapTexture(vars::Vars&vars){
 }
 
 void createShadowMapFBO(vars::Vars&vars){
-  FUNCTION_PROLOGUE("csm","csm.shadowMap");
+  FUNCTION_PROLOGUE("csm.method","csm.method.shadowMap");
 
-  auto fbo = vars.reCreate<Framebuffer>("csm.shadowMapFBO");
-  fbo->attachTexture(GL_DEPTH_ATTACHMENT,vars.get<Texture>("csm.shadowMap"));
+  auto fbo = vars.reCreate<Framebuffer>("csm.method.shadowMapFBO");
+  fbo->attachTexture(GL_DEPTH_ATTACHMENT,vars.get<Texture>("csm.method.shadowMap"));
 }
 
 
 void createShadowMapVAO(vars::Vars&vars){
-  FUNCTION_PROLOGUE("csm","renderModel");
+  FUNCTION_PROLOGUE("csm.method","renderModel");
 
-  auto vao = vars.reCreate<VertexArray>("csm.shadowMapVAO");
+  auto vao = vars.reCreate<VertexArray>("csm.method.shadowMapVAO");
   vao->addAttrib(vars.get<RenderModel>("renderModel")->vertices,0,3,GL_FLOAT);
 }
 
 void createShadowMapProgram(vars::Vars&vars){
-  FUNCTION_PROLOGUE("csm");
+  FUNCTION_PROLOGUE("csm.method");
 
 #include<CubeShadowMapping/CreateShadowMapShaders.h>
-  vars.reCreate<Program>("csm.shadowMapProgram",
+  vars.reCreate<Program>("csm.method.shadowMapProgram",
       make_shared<Shader>(GL_VERTEX_SHADER  ,
         "#version 450\n",
         createShadowMapVertexShaderSource  ),
@@ -51,19 +51,19 @@ void createShadowMapProgram(vars::Vars&vars){
 }
 
 void createMaskFBO(vars::Vars&vars){
-  FUNCTION_PROLOGUE("csm","shadowMask");
+  FUNCTION_PROLOGUE("csm.method","shadowMask");
 
-  auto fbo = vars.reCreate<Framebuffer>("csm.maskFBO");
+  auto fbo = vars.reCreate<Framebuffer>("csm.method.maskFBO");
   fbo->attachTexture(GL_COLOR_ATTACHMENT0,vars.get<Texture>("shadowMask"));
   fbo->drawBuffers(1,GL_COLOR_ATTACHMENT0);
 }
 
 void createShadowMaskProgram(vars::Vars&vars){
-  FUNCTION_PROLOGUE("csm");
+  FUNCTION_PROLOGUE("csm.method");
 
 #include<CubeShadowMapping/ShadowMapToShadowMaskShaders.h>
 
-  vars.reCreate<Program>("csm.shadowMaskProgram",
+  vars.reCreate<Program>("csm.method.shadowMaskProgram",
       make_shared<Shader>(GL_VERTEX_SHADER  ,
         "#version 450\n",
         createShadowMaskVertexShaderSource),
@@ -73,9 +73,9 @@ void createShadowMaskProgram(vars::Vars&vars){
 }
 
 void createShadowMaskVAO(vars::Vars&vars){
-  FUNCTION_PROLOGUE("csm");
+  FUNCTION_PROLOGUE("csm.method");
 
-  vars.reCreate<VertexArray>("csm.maskVAO");
+  vars.reCreate<VertexArray>("csm.method.maskVAO");
 }
 
 CubeShadowMapping::CubeShadowMapping(
@@ -85,7 +85,7 @@ CubeShadowMapping::CubeShadowMapping(
 }
 
 CubeShadowMapping::~CubeShadowMapping(){
-  vars.erase("csm");
+  vars.erase("csm.method");
 }
 
 void CubeShadowMapping::fillShadowMap(glm::vec4 const&lightPosition){
@@ -96,23 +96,28 @@ void CubeShadowMapping::fillShadowMap(glm::vec4 const&lightPosition){
 
   glEnable(GL_POLYGON_OFFSET_FILL);
   glPolygonOffset(2.5,10);
-  auto const resolution = vars.getUint32("args.csm.resolution");
-  auto const near       = vars.getFloat ("args.csm.near"      );
-  auto const far        = vars.getFloat ("args.csm.far"       );
-  auto const faces      = vars.getUint32("args.csm.faces"     );
+  auto const resolution = vars.getUint32("csm.param.resolution");
+  auto const near       = vars.getFloat ("csm.param.near"      );
+  auto const far        = vars.getFloat ("csm.param.far"       );
+  auto const faces      = vars.getUint32("csm.param.faces"     );
+  auto const fbo        = vars.get<Framebuffer>("csm.method.shadowMapFBO");
+  auto const vao        = vars.get<VertexArray>("csm.method.shadowMapVAO");
+  auto const program    = vars.get<Program>("csm.method.shadowMapProgram");
+
+
   glViewport(0,0,resolution,resolution);
   glEnable(GL_DEPTH_TEST);
-  vars.get<Framebuffer>("csm.shadowMapFBO")->bind();
+  fbo->bind();
   glClear(GL_DEPTH_BUFFER_BIT);
-  vars.get<VertexArray>("csm.shadowMapVAO")->bind();
-  vars.get<Program>("csm.shadowMapProgram")
+  vao->bind();
+  program
     ->set4fv("lightPosition",glm::value_ptr(lightPosition))
     ->set1f ("near"         ,near           )
     ->set1f ("far"          ,far            )
     ->use();
   glDrawArraysInstanced(GL_TRIANGLES,0,vars.get<RenderModel>("renderModel")->nofVertices,faces);
-  vars.get<VertexArray>("csm.shadowMapVAO")->unbind();
-  vars.get<Framebuffer>("csm.shadowMapFBO")->unbind();
+  vao->unbind();
+  fbo->unbind();
 }
 
 void CubeShadowMapping::fillShadowMask(glm::vec4 const&lightPosition){
@@ -120,22 +125,22 @@ void CubeShadowMapping::fillShadowMask(glm::vec4 const&lightPosition){
   createMaskFBO(vars);
   createShadowMaskProgram(vars);
 
-  auto const near = vars.getFloat("args.csm.near");
-  auto const far = vars.getFloat("args.csm.far");
+  auto const near = vars.getFloat("csm.param.near");
+  auto const far =  vars.getFloat("csm.param.far");
   auto windowSize = *vars.get<glm::uvec2>("windowSize");
   glViewport(0,0,windowSize.x,windowSize.y);
-  vars.get<Framebuffer>("csm.maskFBO")->bind();
-  vars.get<VertexArray>("csm.maskVAO")->bind();
-  vars.get<Program>("csm.shadowMaskProgram")
+  vars.get<Framebuffer>("csm.method.maskFBO")->bind();
+  vars.get<VertexArray>("csm.method.maskVAO")->bind();
+  vars.get<Program    >("csm.method.shadowMaskProgram")
     ->set4fv("lightPosition",glm::value_ptr(lightPosition))
     ->set1f ("near"         ,near           )
     ->set1f ("far"          ,far            )
     ->use();
   vars.get<GBuffer>("gBuffer")->position->bind(0);
-  vars.get<Texture>("csm.shadowMap")->bind(1);
+  vars.get<Texture>("csm.method.shadowMap")->bind(1);
   glDrawArrays(GL_TRIANGLE_STRIP,0,4);
-  vars.get<VertexArray>("csm.maskVAO")->unbind();
-  vars.get<Framebuffer>("csm.maskFBO")->unbind();
+  vars.get<VertexArray>("csm.method.maskVAO")->unbind();
+  vars.get<Framebuffer>("csm.method.maskFBO")->unbind();
 }
 
 void CubeShadowMapping::create(
