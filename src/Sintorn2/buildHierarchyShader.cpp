6ebe8f677ac23c745901cfe13b24c2f08b189449
Tomáš Milet <amillhouse@seznam.cz>
2019-12-03 03:21:45 +0100

    Sintorn2 - local reduction for X - need testing/debug output

--- src/Sintorn2/buildHierarchyShader.cpp
+++ src/Sintorn2/buildHierarchyShader.cpp
@@ -60,7 +60,52 @@ uint getMorton(uvec2 coord){
 uint activeThread = 0;
 
 shared uint sharedMortons[WARP];
+shared float reductionArray[WARP];
 
+#if WARP == 64
+void reduce(){
+  float ab[2];
+  ab[0] = reductionArray[(uint(gl_LocalInvocationIndex)&0x1fu)+ 0u];
+  ab[1] = reductionArray[(uint(gl_LocalInvocationIndex)&0x1fu)+32u];
+  uint w = uint((ab[1]-ab[0])*(-1.f+2.f*float(uint(gl_LocalInvocationIndex)&1u)) > 0.f);
+  reductionArray[gl_LocalInvocationIndex] = ab[w];
+
+  if((uint(gl_LocalInvocationIndex)&0x10u) == 0u){
+    ab[0] = reductionArray[gl_LocalInvocationIndex +  0u];
+    ab[1] = reductionArray[gl_LocalInvocationIndex + 16u];
+    uint w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(gl_LocalInvocationIndex)&0x20u)!=0)) > 0.f);
+    reductionArray[gl_LocalInvocationIndex - (uint(gl_LocalInvocationIndex)&0x20u)*16u] = ab[w];
+  }
+
+  if((uint(gl_LocalInvocationIndex)&0x28u) == 0u){
+    ab[0] = reductionArray[gl_LocalInvocationIndex + 0u];
+    ab[1] = reductionArray[gl_LocalInvocationIndex + 8u];
+    uint w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(gl_LocalInvocationIndex)&0x10u)!=0)) > 0.f);
+    reductionArray[gl_LocalInvocationIndex - (uint(gl_LocalInvocationIndex)&0x10u)*8u] = ab[w];
+  }
+
+  if((uint(gl_LocalInvocationIndex)&0x34u) == 0u){
+    ab[0] = reductionArray[gl_LocalInvocationIndex + 0u];
+    ab[1] = reductionArray[gl_LocalInvocationIndex + 4u];
+    uint w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(gl_LocalInvocationIndex)&0x8u)!=0)) > 0.f);
+    reductionArray[gl_LocalInvocationIndex - (uint(gl_LocalInvocationIndex)&0x8u)*4u] = ab[w];
+  }
+
+  if((uint(gl_LocalInvocationIndex)&0x3au) == 0u){
+    ab[0] = reductionArray[gl_LocalInvocationIndex + 0u];
+    ab[1] = reductionArray[gl_LocalInvocationIndex + 2u];
+    uint w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(gl_LocalInvocationIndex)&0x4u)!=0)) > 0.f);
+    reductionArray[gl_LocalInvocationIndex - (uint(gl_LocalInvocationIndex)&0x4u)*2u] = ab[w];
+  }
+
+  if((uint(gl_LocalInvocationIndex)&0x3du) == 0u){
+    ab[0] = reductionArray[gl_LocalInvocationIndex + 0u];
+    ab[1] = reductionArray[gl_LocalInvocationIndex + 1u];
+    uint w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(gl_LocalInvocationIndex)&0x2u)!=0)) > 0.f);
+    reductionArray[gl_LocalInvocationIndex - (uint(gl_LocalInvocationIndex)&0x2u)*1u] = ab[w];
+  }
+}
+#endif
 
 void compute(uvec2 coord){
   const uint warpBits        = uint(ceil(log2(float(WARP))));
@@ -164,7 +209,6 @@ void compute(uvec2 coord){
       //uint selectedBit     = (notDone&0xfffffffful)!=0?findLSB(uint(notDone)):findLSB(uint(notDone>>32u))+32u;
       uint referenceMorton = sharedMortons[selectedBit];
 
-      uint64_t sameCluster = ballotARB(referenceMorton == morton && activeThread != 0);
       if(gl_LocalInvocationIndex == 0){
         if(nofLevels>0){
           uint bit  = (referenceMorton >> (warpBits*0u)) & warpMask;
@@ -196,20 +240,27 @@ void compute(uvec2 coord){
           uint node = (referenceMorton >> (warpBits*6u));
           atomicOr(nodePool[levelOffset[clamp(nofLevels-6u,0u,5u)]+node*2u+uint(bit>31u)],1u<<(bit&0x1fu));
         }
-
-
-
-        //if(nofLevels>1){
-        //  uint bit  = (referenceMorton >> (warpBits*1ul)) & warpMask;
-        //  uint node = (referenceMorton >> (warpBits*2ul));
-        //  atomicOr(nodePool[levelOffset[clamp(nofLevels-2u,0u,5u)]+node*2u+uint(bit>31u)],1u<<(bit&0x1fu));
-        //}
-        //if(nofLevels>1){
-        //  uint bit  = (referenceMorton >> (warpBits*1ul)) & warpMask;
-        //  uint node = (referenceMorton >> (warpBits*2ul));
-        //  atomicOr(nodePool[levelOffset[clamp(nofLevels-2u,0u,5u)]+node*2u+uint(bit>31u)],1u<<(bit&0x1fu));
-        //}
       }
+      
+      uint64_t sameCluster = ballotARB(referenceMorton == morton && activeThread != 0);
+
+
+
+      reductionArray[gl_LocalInvocationIndex] = -1.f + 2.f/float(WINDOW_X)*(coord.x+0.5f);
+
+      if(referenceMorton != morton || activeThread == 0)
+        reductionArray[gl_LocalInvocationIndex] = reductionArray[selectedBit];
+
+      reduce();
+
+      if(gl_LocalInvocationIndex == 0){
+        uint bit  = (referenceMorton >> (warpBits*0u)) & warpMask;
+        uint node = (referenceMorton >> (warpBits*1u));
+        aabbPool[levelOffset[clamp(nofLevels-1u,0u,5u)]*3u+node*6u+0] = reductionArray[0];
+        aabbPool[levelOffset[clamp(nofLevels-1u,0u,5u)]*3u+node*6u+1] = reductionArray[1];
+      }
+
+
 
       notDone ^= sameCluster;
     }
