14ca8de2201072c862258f2aced0149872558f4a
Tomáš Milet <amillhouse@seznam.cz>
2019-12-08 15:08:27 +0100

    Sintorn2 - implementing shadow frusta extraction

--- src/Sintorn2/computeShadowFrusta.cpp
+++ src/Sintorn2/computeShadowFrusta.cpp
@@ -1,6 +1,18 @@
+#include <glm/glm.hpp>
+
 #include <Vars/Vars.h>
+#include <geGL/geGL.h>
+
+#include <FunctionPrologue.h>
 
 #include <Sintorn2/computeShadowFrusta.h>
+
+namespace sintorn2{
+void createComputeShadowFrustaProgram(vars::Vars&vars){
+
+}
+
+}
 
 void sintorn2::computeShadowFrusta(vars::Vars&vars){
 }
