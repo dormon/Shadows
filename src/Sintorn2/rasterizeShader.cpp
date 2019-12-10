f07cbcfa0a3e5bd3ade1aee470a5765a766f2b47
Tomáš Milet <amillhouse@seznam.cz>
2019-12-10 19:30:43 +0100

    Sintorn2 - reoreding rasterization algorithm

--- src/Sintorn2/rasterizeShader.cpp
+++ src/Sintorn2/rasterizeShader.cpp
@@ -19,8 +19,9 @@ std::string const sintorn2::rasterizeShader = R".(
 #endif//SF_INTERLEAVE
 
 #define PLANES_PER_SF 4
-#define FLOATS_PER_PLANE 4
-#define FLOATS_PER_SF (PLANES_PER_SF * FLOATS_PER_PLANE)
+
+const uint floatsPerPlane = 4;
+const uint floatsPerSF    = floatsPerPlane * PLANES_PER_SF;
 
 layout(local_size_x=WARP)in;
 
@@ -29,20 +30,23 @@ layout(std430,binding=1)buffer AABBPool    {float aabbPool    [];};
 layout(std430,binding=2)buffer ShadowFrusta{float shadowFrusta[];};
 layout(std430,binding=3)buffer JobCounter  {uint  jobCounter  [];};
 
+layout(     binding=0)          uniform sampler2DRect depthTexture;
+layout(r32f,binding=1)writeonly uniform image2D       shadowMask  ;
+
 
 const uint alignedNofSF = (uint(NOF_TRIANGLES / SF_ALIGNMENT) + uint((NOF_TRIANGLES % SF_ALIGNMENT) != 0u)) * SF_ALIGNMENT;
 
 void loadShadowFrustum(uint job){
-  if(gl_LocalInvocationIndex < FLOATS_PER_SF){
+  if(gl_LocalInvocationIndex < floatsPerSF){
 #if TRIANGLE_INTERLEAVE == 1
     shadowFrustaPlanes[gl_LocalInvocationIndex] = shadowFrusta[alignedNofSF*gl_LocalInvocationIndex + job];
 #else
-    shadowFrustaPlanes[gl_LocalInvocationIndex] = shadowFrusta[job*FLOATS_PER_SF+gl_LocalInvocationIndex];
+    shadowFrustaPlanes[gl_LocalInvocationIndex] = shadowFrusta[job*floatsPerSF+gl_LocalInvocationIndex];
 #endif
   }
 }
 
-shared float shadowFrustaPlanes[FLOATS_PER_SF];
+shared float shadowFrustaPlanes[floatsPerSF];
 
 vec3 trivialRejectCorner3D(vec3 Normal){
   return vec3((ivec3(sign(Normal))+1)/2);
@@ -59,34 +63,55 @@ uint trivialRejectAccept(vec3 minCorner,vec3 size){
 
   plane = vec4(shadowFrustaPlanes[0],shadowFrustaPlanes[1],shadowFrustaPlanes[2],shadowFrustaPlanes[3]);
   tr    = trivialRejectCorner3D(plane.xyz);
-  if(dot(plane,vec4(minCorner + tr*size,1))<0)
+  if(dot(plane,vec4(minCorner + tr*size,1.f))<0.f)
     return TRIVIAL_REJECT;
-  tr = 1-tr;
-  status &= uint(dot(vec4(minCorner + tr*size,1),plane)>0);
+  tr = 1.f-tr;
+  status &= uint(dot(vec4(minCorner + tr*size,1),plane)>0.f);
 
   plane = vec4(shadowFrustaPlanes[4],shadowFrustaPlanes[5],shadowFrustaPlanes[6],shadowFrustaPlanes[7]);
   tr    = trivialRejectCorner3D(plane.xyz);
-  if(dot(plane,vec4(minCorner + tr*size,1))<0)
+  if(dot(plane,vec4(minCorner + tr*size,1.f))<0.f)
     return TRIVIAL_REJECT;
-  tr = 1-tr;
-  status &= uint(dot(vec4(minCorner + tr*size,1),plane)>0);
+  tr = 1.f-tr;
+  status &= uint(dot(vec4(minCorner + tr*size,1),plane)>0.f);
 
   plane = vec4(shadowFrustaPlanes[8],shadowFrustaPlanes[9],shadowFrustaPlanes[10],shadowFrustaPlanes[11]);
   tr    = trivialRejectCorner3D(plane.xyz);
-  if(dot(plane,vec4(minCorner + tr*size,1))<0)
+  if(dot(plane,vec4(minCorner + tr*size,1.f))<0.f)
     return TRIVIAL_REJECT;
-  tr = 1-tr;
-  status &= uint(dot(vec4(minCorner + tr*size,1),plane)>0);
+  tr = 1.f-tr;
+  status &= uint(dot(vec4(minCorner + tr*size,1),plane)>0.f);
 
   plane = vec4(shadowFrustaPlanes[12],shadowFrustaPlanes[13],shadowFrustaPlanes[14],shadowFrustaPlanes[15]);
   tr    = trivialRejectCorner3D(plane.xyz);
-  if(dot(plane,vec4(minCorner + tr*size,1))<0)
+  if(dot(plane,vec4(minCorner + tr*size,1.f))<0.f)
     return TRIVIAL_REJECT;
-  tr = 1-tr;
-  status &= uint(dot(vec4(minCorner + tr*size,1),plane)>0);
+  tr = 1.f-tr;
+  status &= uint(dot(vec4(minCorner + tr*size,1.f),plane)>0.f);
 
   return status;
 }
+
+/*
+#define TEST_SHADOW_FRUSTUM_LAST(LEVEL)\
+void JOIN(TestShadowFrustumHDB,LEVEL)(uvec2 coord,vec2 clipCoord){\
+  uvec2 localCoord             = getLocalCoords(LEVEL);\
+  uvec2 globalCoord            = coord * JOIN(TILE_DIVISIBILITY,LEVEL) + localCoord;\
+  vec2  globalClipCoord        = clipCoord + JOIN(TILE_SIZE_IN_CLIP_SPACE,LEVEL) * localCoord;\
+  if(texelFetch(triangleID,ivec2(globalCoord),0).x == SHADOWFRUSTUM_ID_IN_DISPATCH)return;\
+  vec4  SampleCoordInClipSpace = vec4(\
+    globalClipCoord + JOIN(TILE_SIZE_IN_CLIP_SPACE,LEVEL)*.5,\
+    texelFetch(HDT[LEVEL],ivec2(globalCoord),0).x,1);\
+  if(SampleCoordInClipSpace.z >= 1)return;\
+  bool inside = true;\
+  for(int p = 0; p < NOF_PLANES_PER_SF; ++p)\
+    inside=inside && dot(SampleCoordInClipSpace,SharedShadowFrusta[SHADOWFRUSTUM_ID_IN_WORKGROUP][p])>=0;\
+  if(inside)\
+    imageStore(FinalStencilMask,ivec2(globalCoord),uvec4(SHADOW_VALUE));\
+}
+
+*/
+//imageStore(shadowMask,ivec2(gl_GlobalInvocationID.xy),vec4(1-S));
 
 void traverse(){
   int level = 0;
@@ -96,18 +121,19 @@ void traverse(){
   while(level >= 0){
     if(level == int(nofLevels)){
       //test pixels
+      node >>= warpBits;
       level--;
     }else{
-      uint status = uint(nodePool[nodeLevelOffsetInUints[level]+ node*uintsPerWarp + uint(gl_LocalInvocationIndex>31)]&uint(1u<<(gl_LocalInvocationIndex&0x1fu)));
+      uint status = uint(nodePool[nodeLevelOffsetInUints[level]+ node*uintsPerWarp + uint(gl_LocalInvocationIndex>31u)]&uint(1u<<(gl_LocalInvocationIndex&0x1fu)));
       if(status != 0u){
         vec3 minCorner;
         vec3 aabbSize;
-        minCorner[0] = aabbPool[aabbLevelOffsetInFloats[level]+uint(gl_LocalInvocationIndex)*6+0]             ;
-        minCorner[1] = aabbPool[aabbLevelOffsetInFloats[level]+uint(gl_LocalInvocationIndex)*6+2]             ;
-        minCorner[2] = aabbPool[aabbLevelOffsetInFloats[level]+uint(gl_LocalInvocationIndex)*6+4]             ;
-        aabbSize [0] = aabbPool[aabbLevelOffsetInFloats[level]+uint(gl_LocalInvocationIndex)*6+1]-minCorner[0];
-        aabbSize [1] = aabbPool[aabbLevelOffsetInFloats[level]+uint(gl_LocalInvocationIndex)*6+3]-minCorner[1];
-        aabbSize [2] = aabbPool[aabbLevelOffsetInFloats[level]+uint(gl_LocalInvocationIndex)*6+5]-minCorner[2];
+        minCorner[0] = aabbPool[aabbLevelOffsetInFloats[level] + node*WARP*6u + gl_LocalInvocationIndex*6u + 0u]             ;
+        minCorner[1] = aabbPool[aabbLevelOffsetInFloats[level] + node*WARP*6u + gl_LocalInvocationIndex*6u + 2u]             ;
+        minCorner[2] = aabbPool[aabbLevelOffsetInFloats[level] + node*WARP*6u + gl_LocalInvocationIndex*6u + 4u]             ;
+        aabbSize [0] = aabbPool[aabbLevelOffsetInFloats[level] + node*WARP*6u + gl_LocalInvocationIndex*6u + 1u]-minCorner[0];
+        aabbSize [1] = aabbPool[aabbLevelOffsetInFloats[level] + node*WARP*6u + gl_LocalInvocationIndex*6u + 3u]-minCorner[1];
+        aabbSize [2] = aabbPool[aabbLevelOffsetInFloats[level] + node*WARP*6u + gl_LocalInvocationIndex*6u + 5u]-minCorner[2];
 
         status = trivialRejectAccept(minCorner,aabbSize);
       }
@@ -115,19 +141,21 @@ void traverse(){
       uint64_t trA        = ballotARB(status == TRIVIAL_ACCEPT);
 
 
-      if(gl_LocalInvocationIndex == 0){
-        if(unpackUint2x32(trA)[0] != 0)
+      if(gl_LocalInvocationIndex == 0u){
+        if(unpackUint2x32(trA)[0] != 0u)
           atomicAnd(nodePool[nodeLevelOffsetInUints[level]+node*uintsPerWarp+0],~unpackUint2x32(trA)[0]);
-        if(unpackUint2x32(trA)[1] != 0)
+        if(unpackUint2x32(trA)[1] != 0u)
           atomicAnd(nodePool[nodeLevelOffsetInUints[level]+node*uintsPerWarp+1],~unpackUint2x32(trA)[1]);
       }
     }
 
     if(intersection[level] == 0){
+      node >>= warpBits;
       level--;
     }else{
       uint selectedBit = unpackUint2x32(intersection[level])[0]!=0?findLSB(unpackUint2x32(intersection[level])[0]):findLSB(unpackUint2x32(intersection[level])[1])+32u;
-      node = node*WARP+selectedBit;
+      node <<= warpBits   ;
+      node  += selectedBit;
 
       intersection[level] ^= 1u << selectedBit;
       level++;
