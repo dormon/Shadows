6468dfe4b1f1dbe519b8cff43bdb5602adbdaf9c
Tomáš Milet <amillhouse@seznam.cz>
2019-12-12 14:50:50 +0100

    Sintorn2 - fixing rare bug in morton / demorton shader - L and H mask now have 0 between them

--- src/Sintorn2/mortonShader.cpp
+++ src/Sintorn2/mortonShader.cpp
@@ -70,28 +70,34 @@ uint morton2(uvec3 v){
 uint morton(uvec3 v){
   //return morton2(v);
 
-  const uint shortestAxis  = uint(uint(bitLength[0] == yBits) + uint(bitLength[0] == zBits)*2u); //uint(uint(shortest == yBits) + uint(shortest == zBits)*2u);
-  const uint longestAxis   = clamp(uint(uint(bitLength[2]  == yBits) + uint(bitLength[2]  == zBits)*2u),0u,2u);//clamp(uint(uint(longest  == yBits) + uint(longest  == zBits)*2u),0u,2u);
+  const uint shortest      = min(min(xBits,yBits),zBits);
+  const uint middle        = max(max(min(xBits,yBits),min(xBits,zBits)),min(yBits,zBits));
+  const uint longest       = max(max(xBits,yBits),zBits);
+  const uint bits3Length   = shortest;
+  const uint bits2Length   = uint(middle-shortest);
+  const uint bits1Length   = uint(longest-middle);
+  const uint shortestAxis  = uint(uint(shortest == yBits) + uint(shortest == zBits)*2u);
+  const uint longestAxis   = clamp(uint(uint(longest  == yBits) + uint(longest  == zBits)*2u),0u,2u);
   const uint shortestZ     = uint(shortestAxis == 2u);
   const uint shortestY     = uint(shortestAxis == 1u);
   const uint longestZ      = uint(longestAxis == 2u);
   const uint longestX      = uint(longestAxis == 0u);
-  const uint isMiddle      = uint(bitTogether[1] > 0u);
-  const uint isLongest     = uint(bitTogether[2] > 0u);
-  const uint bits2Shifts   = uint(uint(bitTogether[1] - uint(shortestZ | (shortestY & longestZ))) * isMiddle);
+  const uint isMiddle      = uint(bits2Length > 0u);
+  const uint isLongest     = uint(bits1Length > 0u);
+  const uint bits2Shifts   = uint(uint(bits2Length - uint(shortestZ | (shortestY & longestZ))) * isMiddle);
 
-  const uint bits2OffsetB   = bitTogether[0]*3u + shortestAxis;
-  const uint bits2Offset00  = bits2OffsetB + 2u* 0u;
-  const uint bits2Offset01  = bits2OffsetB + 2u* 1u;
-  const uint bits2Offset02  = bits2OffsetB + 2u* 2u;
-  const uint bits2Offset03  = bits2OffsetB + 2u* 3u;
-  const uint bits2Offset04  = bits2OffsetB + 2u* 4u;
-  const uint bits2Offset05  = bits2OffsetB + 2u* 5u;
-  const uint bits2Offset06  = bits2OffsetB + 2u* 6u;
-  const uint bits2Offset07  = bits2OffsetB + 2u* 7u;
-  const uint bits2Offset08  = bits2OffsetB + 2u* 8u;
-  const uint bits2Offset09  = bits2OffsetB + 2u* 9u;
-  const uint bits2Offset10  = bits2OffsetB + 2u*10u;
+  const uint bits2OffsetB   = bits3Length*3u + shortestAxis;
+  const uint bits2Offset00  = bits2OffsetB + 2* 0;
+  const uint bits2Offset01  = bits2OffsetB + 2* 1;
+  const uint bits2Offset02  = bits2OffsetB + 2* 2;
+  const uint bits2Offset03  = bits2OffsetB + 2* 3;
+  const uint bits2Offset04  = bits2OffsetB + 2* 4;
+  const uint bits2Offset05  = bits2OffsetB + 2* 5;
+  const uint bits2Offset06  = bits2OffsetB + 2* 6;
+  const uint bits2Offset07  = bits2OffsetB + 2* 7;
+  const uint bits2Offset08  = bits2OffsetB + 2* 8;
+  const uint bits2Offset09  = bits2OffsetB + 2* 9;
+  const uint bits2Offset10  = bits2OffsetB + 2*10;
 
   const uint bits2LMask00 = uint((1u << bits2Offset00)-1u);
   const uint bits2LMask01 = uint((1u << bits2Offset01)-1u);
@@ -105,17 +111,24 @@ uint morton(uvec3 v){
   const uint bits2LMask09 = uint((1u << bits2Offset09)-1u);
   const uint bits2LMask10 = uint((1u << bits2Offset10)-1u);
 
-  const uint bits2HMask00 = ~bits2LMask00;
-  const uint bits2HMask01 = ~bits2LMask01;
-  const uint bits2HMask02 = ~bits2LMask02;
-  const uint bits2HMask03 = ~bits2LMask03;
-  const uint bits2HMask04 = ~bits2LMask04;
-  const uint bits2HMask05 = ~bits2LMask05;
-  const uint bits2HMask06 = ~bits2LMask06;
-  const uint bits2HMask07 = ~bits2LMask07;
-  const uint bits2HMask08 = ~bits2LMask08;
-  const uint bits2HMask09 = ~bits2LMask09;
-  const uint bits2HMask10 = ~bits2LMask10;
+  const uint bits2HMask00 = (~bits2LMask00)<<1u;
+  const uint bits2HMask01 = (~bits2LMask01)<<1u;
+  const uint bits2HMask02 = (~bits2LMask02)<<1u;
+  const uint bits2HMask03 = (~bits2LMask03)<<1u;
+  const uint bits2HMask04 = (~bits2LMask04)<<1u;
+  const uint bits2HMask05 = (~bits2LMask05)<<1u;
+  const uint bits2HMask06 = (~bits2LMask06)<<1u;
+  const uint bits2HMask07 = (~bits2LMask07)<<1u;
+  const uint bits2HMask08 = (~bits2LMask08)<<1u;
+  const uint bits2HMask09 = (~bits2LMask09)<<1u;
+  const uint bits2HMask10 = (~bits2LMask10)<<1u;
+
+
+  const uint bits1Count    = uint(bits1Length - uint(shortestY & longestX) + uint(shortestY & longestZ)) * isLongest;
+  const uint bits1used     = longest - bits1Count;
+  const uint bits1DstMask  = uint((1u<<(bits3Length*3u + bits2Length*2u + uint(shortestY & longestX) - uint(longestZ & shortestY))) -1u);
+  const uint bits1SrcShift = bits3Length*3u + bits2Length*2u - uint(shortestY & longestZ) + uint(shortestY & longestX)  - bits1used;
+  const uint bits1SrcMask  = ~((1u<<bits1used)-1u);
 
   uint res = 0;
   uint vv;
@@ -146,13 +159,6 @@ uint morton(uvec3 v){
   if(9  < bits2Shifts)res = ((res & bits2HMask09)>>1u) | (res & bits2LMask09);
   if(10 < bits2Shifts)res = ((res & bits2HMask10)>>1u) | (res & bits2LMask10);
   
-  const uint bits1Count    = uint(bitTogether[2] - uint(shortestY & longestX) + uint(shortestY & longestZ)) * isLongest;
-  const uint bits1used     = bitLength[2] - bits1Count;//longest - bits1Count;
-  const uint bits1DstMask  = uint((1u<<(bitTogether[0]*3u + bitTogether[1]*2u + uint(shortestY & longestX)/*- longestZ*/)) -1u);
-  const uint bits1SrcShift = bitTogether[0]*3u + bitTogether[1]*2u - uint(shortestY & longestZ) + uint(shortestY & longestX)  - bits1used;
-  const uint bits1SrcMask  = ~((1u<<bits1used)-1u);
-
-
   if(bits1Count != 0)
     res = uint(res & bits1DstMask) | uint((v[longestAxis]&bits1SrcMask)<<bits1SrcShift);
 
