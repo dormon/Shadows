08c5dc1f3ff1f434dd60c493b5fbdd36a01faad4
dormon <imilet@fit.vutbr.cz>
2019-12-14 01:06:40 +0100

    Sintorn2 - NVIDIA build hierarchy

--- src/Sintorn2/propagateAABBShader.cpp
+++ src/Sintorn2/propagateAABBShader.cpp
@@ -50,12 +50,66 @@ layout(std430,binding=7)buffer DebugBuffer{uint debugBuffer[];};
 
 uniform uint destLevel = 0;
 
+#if WARP == 32
+
+shared float reductionArray[WARP*6u*NOF_WARPS];
+
+void reduce(){
+  float ab[2];
+  uint w;
+
+  //6*32 -> 6*16
+  ab[0] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&16u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*0u + THREAD_IN_WARP] = ab[w];
+  
+  ab[0] = reductionArray[WARP_OFFSET+WARP*2u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*2u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&16u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*1u + THREAD_IN_WARP] = ab[w];
+
+  ab[0] = reductionArray[WARP_OFFSET+WARP*4u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*4u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&16u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*2u + THREAD_IN_WARP] = ab[w];
+  
+  //6*16 -> 6*8
+  ab[0] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&8u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*0u + THREAD_IN_WARP] = ab[w];
+  
+  ab[0] = reductionArray[WARP_OFFSET+WARP*2u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*2u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&8u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*1u + THREAD_IN_WARP] = ab[w];
+
+  //6*8 -> 6*4
+  ab[0] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&4u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*0u + THREAD_IN_WARP] = ab[w];
+
+  //6*4 -> 6*2
+  ab[0] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&2u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*0u + THREAD_IN_WARP] = ab[w];
+
+  //6*2 -> 6*1
+  ab[0] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 0u];                 
+  ab[1] = reductionArray[WARP_OFFSET+WARP*0u + (uint(THREAD_IN_WARP)<<1u) + 1u];                 
+  w = uint((ab[1]-ab[0])*(-1.f+2.f*float((uint(THREAD_IN_WARP)&1u)!=0u)) > 0.f);
+  reductionArray[WARP_OFFSET+WARP*0u + THREAD_IN_WARP] = ab[w];
+
+}
+
+#endif
 
 #if WARP == 64
 
 shared float reductionArray[WARP*6u*NOF_WARPS];
-
-
 
 void reduce(){
   float ab[2];
@@ -116,6 +170,84 @@ void reduce(){
 #endif
 #line 110
 void main(){
+
+#if WARP == 32
+
+  uint wid = gl_WorkGroupID.x + gl_WorkGroupID.y*gl_NumWorkGroups.x;
+
+  uint activeNodeUint = activeNodes[nodeLevelOffset[destLevel]+wid];
+  uint nodeUint = nodePool[nodeLevelOffsetInUints[destLevel]+activeNodeUint];
+
+#if NOF_WARPS > 1
+  nodeUint &= uint((1u<<uint(32/NOF_WARPS))-1u) << (uint(32/NOF_WARPS)*WARP_ID);
+#endif
+
+
+#ifdef BE_SAFE
+  uint counter = 0;
+#endif
+
+  while(nodeUint != 0){
+
+#ifdef BE_SAFE
+    if(counter >=32)break;
+#endif
+
+    uint bit = findLSB(nodeUint);
+    uint node = activeNodeUint*WARP + bit;
+
+    uint isActive = uint(nodePool[nodeLevelOffsetInUints[destLevel+1u]+node*uintsPerWarp + uint(THREAD_IN_WARP>31)] & uint(1u<<(uint(THREAD_IN_WARP)&0x1fu)));
+
+    uint64_t activeThreads = ballotARB(isActive != 0);
+    uint selectedBit       = findLSB(unpackUint2x32(activeThreads)[0]);
+
+    if(isActive != 0){
+      reductionArray[WARP_OFFSET+WARP*0u+uint(THREAD_IN_WARP)] = aabbPool[aabbLevelOffsetInFloats[destLevel+1]+node*WARP*floatsPerAABB+uint(THREAD_IN_WARP)*floatsPerAABB+0u];
+      reductionArray[WARP_OFFSET+WARP*1u+uint(THREAD_IN_WARP)] = aabbPool[aabbLevelOffsetInFloats[destLevel+1]+node*WARP*floatsPerAABB+uint(THREAD_IN_WARP)*floatsPerAABB+1u];
+      reductionArray[WARP_OFFSET+WARP*2u+uint(THREAD_IN_WARP)] = aabbPool[aabbLevelOffsetInFloats[destLevel+1]+node*WARP*floatsPerAABB+uint(THREAD_IN_WARP)*floatsPerAABB+2u];
+      reductionArray[WARP_OFFSET+WARP*3u+uint(THREAD_IN_WARP)] = aabbPool[aabbLevelOffsetInFloats[destLevel+1]+node*WARP*floatsPerAABB+uint(THREAD_IN_WARP)*floatsPerAABB+3u];
+      reductionArray[WARP_OFFSET+WARP*4u+uint(THREAD_IN_WARP)] = aabbPool[aabbLevelOffsetInFloats[destLevel+1]+node*WARP*floatsPerAABB+uint(THREAD_IN_WARP)*floatsPerAABB+4u];
+      reductionArray[WARP_OFFSET+WARP*5u+uint(THREAD_IN_WARP)] = aabbPool[aabbLevelOffsetInFloats[destLevel+1]+node*WARP*floatsPerAABB+uint(THREAD_IN_WARP)*floatsPerAABB+5u];
+    }
+
+    if(isActive == 0){
+      reductionArray[WARP_OFFSET+WARP*0u+uint(THREAD_IN_WARP)] = reductionArray[WARP_OFFSET+WARP*0u+selectedBit];
+      reductionArray[WARP_OFFSET+WARP*1u+uint(THREAD_IN_WARP)] = reductionArray[WARP_OFFSET+WARP*1u+selectedBit];
+      reductionArray[WARP_OFFSET+WARP*2u+uint(THREAD_IN_WARP)] = reductionArray[WARP_OFFSET+WARP*2u+selectedBit];
+      reductionArray[WARP_OFFSET+WARP*3u+uint(THREAD_IN_WARP)] = reductionArray[WARP_OFFSET+WARP*3u+selectedBit];
+      reductionArray[WARP_OFFSET+WARP*4u+uint(THREAD_IN_WARP)] = reductionArray[WARP_OFFSET+WARP*4u+selectedBit];
+      reductionArray[WARP_OFFSET+WARP*5u+uint(THREAD_IN_WARP)] = reductionArray[WARP_OFFSET+WARP*5u+selectedBit];
+    }
+
+    reduce();
+
+    if(THREAD_IN_WARP < floatsPerAABB)
+      aabbPool[aabbLevelOffsetInFloats[destLevel]+node*floatsPerAABB+THREAD_IN_WARP] = reductionArray[WARP_OFFSET+THREAD_IN_WARP];
+
+    nodeUint ^= 1u << bit;
+
+#ifdef BE_SAFE
+    counter++;
+#endif
+  }
+
+  if(gl_LocalInvocationIndex == 0){
+
+    uint bit  = activeNodeUint& warpMask;
+    uint node = activeNodeUint>>warpBits;
+
+    if(destLevel > 0){
+      uint mm = atomicOr(nodePool[nodeLevelOffsetInUints[destLevel-1]+node*uintsPerWarp+uint(bit>31u)],1u<<(bit&0x1fu));
+      if(mm == 0){
+        mm = atomicAdd(levelNodeCounter[(destLevel-1)*4u],1);
+        activeNodes[nodeLevelOffset[destLevel-1]+mm] = node*uintsPerWarp+uint(bit>31u);
+      }
+    }
+  }
+
+
+#endif
+
 
 #line 141
 #if WARP == 64
