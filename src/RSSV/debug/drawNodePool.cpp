bc3505b95b0d0341179b8631fb3658eecf839904
Tomáš Milet <amillhouse@seznam.cz>
2019-12-30 17:49:08 +0100

    RSSV - simplified drawNODE

--- src/RSSV/debug/drawNodePool.cpp
+++ src/RSSV/debug/drawNodePool.cpp
@@ -116,11 +116,6 @@ void prepareDrawNodePool(vars::Vars&vars){
 #define FOVY 1.5707963267948966f
 #endif//FOVY
 
-uint divRoundUp(uint x,uint y){
-  return uint(x/y) + uint((x%y)>0);
-}
-
-
 layout(points)in;
 
 #if WIREFRAME == 1
@@ -141,31 +136,12 @@ uniform mat4 proj;
 uniform mat4 nodeView;
 uniform mat4 nodeProj;
 
-#line 122
 uniform uint levelToDraw = 0;
 
 uniform uint drawTightAABB = 0;
 
-uniform uint usePrecomputedSize = 0;
-
-/*
-void getClipAABB(inout vec3 minCorner,inout vec3 maxCorner,uint node,uint level){
-  uvec3 coord = demorton(node << (warpBits*(nofLevels-1-level)));
-  minCorner.xy = -1.f + 2.f*vec2(coord.xy << uvec2(tileBitsX,tileBitsY)) / vec2(WINDOW_X,WINDOW_Y);
-  minCorner.z  = clusterToZ(coord.z);
-
-  const uint xBitsAvail[] = {
-    0,
-
-  };
-  
-  
-}
-*/
-
 void main(){
   uint gId = vId[0];
-#line 157
 
   float mminX;
   float mmaxX;
@@ -181,71 +157,27 @@ void main(){
   float startZ;
   float endZ  ;
 
+  uvec3 xyz = demorton(gId<<(warpBits*(nofLevels-1-levelToDraw)));
 
- 
+  uint bit  = gId & warpMask;
+  uint node = gId >> warpBits;
 
-  if(usePrecomputedSize == 1u){
-    uvec3 xyz = demorton(gId<<(warpBits*(nofLevels-1-levelToDraw)));
+  uint doesNodeExist = nodePool[nodeLevelOffsetInUints[clamp(levelToDraw,0u,5u)]+node*uintsPerWarp+uint(bit>31u)]&(1u<<(bit&0x1fu));
+  if(doesNodeExist == 0)return;
 
-    uint bit  = gId & warpMask;
-    uint node = gId >> warpBits;
+  mminX = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+0];
+  mmaxX = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+1];
+  mminY = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+2];
+  mmaxY = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+3];
+  mminZ = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+4];
+  mmaxZ = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+5];
 
-    uint doesNodeExist = nodePool[nodeLevelOffsetInUints[clamp(levelToDraw,0u,5u)]+node*uintsPerWarp+uint(bit>31u)]&(1u<<(bit&0x1fu));
-    if(doesNodeExist == 0)return;
-
-    mminX = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+0];
-    mmaxX = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+1];
-    mminY = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+2];
-    mmaxY = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+3];
-    mminZ = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+4];
-    mmaxZ = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+gId*floatsPerAABB+5];
-
-    startX = -1.f + xyz.x*levelTileSizeClipSpace[nofLevels-1].x;
-    startY = -1.f + xyz.y*levelTileSizeClipSpace[nofLevels-1].y;
-    endX   = min(startX + levelTileSizeClipSpace[levelToDraw].x,1.f);
-    endY   = min(startY + levelTileSizeClipSpace[levelToDraw].y,1.f);
-    startZ = CLUSTER_TO_Z(xyz.z                                   );
-    endZ   = CLUSTER_TO_Z(xyz.z+(1u<<levelTileBits[levelToDraw].z));
-
-  }else{
-
-    uint bitsToDiv = warpBits*(nofLevels-1-levelToDraw);
-    uint xBitsToDiv = divRoundUp(bitsToDiv , 3u);
-    uint yBitsToDiv = divRoundUp(uint(max(int(bitsToDiv)-1,0)) , 3u);
-    uint zBitsToDiv = divRoundUp(uint(max(int(bitsToDiv)-2,0)) , 3u);
-
-    uint clusX = divRoundUp(clustersX,1u<<xBitsToDiv);
-    uint clusY = divRoundUp(clustersY,1u<<yBitsToDiv);
-
-    uint x = gId % clusX;
-    uint y = (gId / clusX) % clusY;
-    uint z = (gId / (clusX * clusY));
-
-    uint mor = morton(uvec3(x<<xBitsToDiv,y<<yBitsToDiv,z<<zBitsToDiv));
-    uint bit  = (mor >> (warpBits*(nofLevels-1-levelToDraw))) & warpMask;
-    uint node = (mor >> (warpBits*(nofLevels  -levelToDraw)));
-
-
-    uint doesNodeExist = nodePool[nodeLevelOffsetInUints[clamp(levelToDraw,0u,5u)]+node*uintsPerWarp+uint(bit>31u)]&(1u<<(bit&0x1fu));
-
-    if(doesNodeExist == 0)return;
-
-    uint aabbNode = (mor >> (warpBits*(nofLevels-1-levelToDraw)));
-    mminX = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+aabbNode*floatsPerAABB+0];
-    mmaxX = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+aabbNode*floatsPerAABB+1];
-    mminY = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+aabbNode*floatsPerAABB+2];
-    mmaxY = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+aabbNode*floatsPerAABB+3];
-    mminZ = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+aabbNode*floatsPerAABB+4];
-    mmaxZ = aabbPool[aabbLevelOffsetInFloats[clamp(levelToDraw,0u,5u)]+aabbNode*floatsPerAABB+5];
-
-    startZ = CLUSTER_TO_Z((z  )<<levelTileBits[levelToDraw].z);
-    endZ   = CLUSTER_TO_Z((z+1)<<levelTileBits[levelToDraw].z);
-    startX = -1.f + x*levelTileSizeClipSpace[levelToDraw].x;
-    startY = -1.f + y*levelTileSizeClipSpace[levelToDraw].y;
-    endX   = clamp(-1.f + (x+1)*levelTileSizeClipSpace[levelToDraw].x,-1.f,1.f);
-    endY   = clamp(-1.f + (y+1)*levelTileSizeClipSpace[levelToDraw].y,-1.f,1.f);
-  }
-
+  startX = -1.f + xyz.x*levelTileSizeClipSpace[nofLevels-1].x;
+  startY = -1.f + xyz.y*levelTileSizeClipSpace[nofLevels-1].y;
+  endX   = min(startX + levelTileSizeClipSpace[levelToDraw].x,1.f);
+  endY   = min(startY + levelTileSizeClipSpace[levelToDraw].y,1.f);
+  startZ = CLUSTER_TO_Z(xyz.z                                   );
+  endZ   = CLUSTER_TO_Z(xyz.z+(1u<<levelTileBits[levelToDraw].z));
 
 #ifdef FAR_IS_INFINITE
   float e = -1.f;
@@ -393,7 +325,6 @@ void drawNodePool(vars::Vars&vars){
   auto vao = vars.get<VertexArray>("rssv.method.debug.vao");
 
   auto prg = vars.get<Program>("rssv.method.debug.drawNodePoolProgram");
-  auto usePrecomputedSize = vars.getBool("rssv.method.debug.usePrecomputedSize");
 
   vao->bind();
   nodePool->bindBase(GL_SHADER_STORAGE_BUFFER,0);
@@ -405,7 +336,6 @@ void drawNodePool(vars::Vars&vars){
     ->setMatrix4fv("view"       ,glm::value_ptr(view    ))
     ->setMatrix4fv("proj"       ,glm::value_ptr(proj    ))
     ->set1ui      ("drawTightAABB",(uint32_t)drawTightAABB)
-    ->set1ui      ("usePrecomputedSize",(uint32_t)usePrecomputedSize)
     ;
 
 
