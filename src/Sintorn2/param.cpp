fcddecc1f9c17ec354035ebf8a9e33686f3f800b
Tomáš Milet <amillhouse@seznam.cz>
2020-01-21 18:22:05 +0100

    Sintorn2 - minZparam - 10

--- src/Sintorn2/param.cpp
+++ src/Sintorn2/param.cpp
@@ -7,7 +7,7 @@ void sintorn2::loadParams(
     std::shared_ptr<argumentViewer::ArgumentViewer>const&arg){
   auto c = arg->getContext("sintorn2Param","parameters for Per-Triangle Shadow Volumes Using a View-Sample Cluster Hierarchy method");
 
-  vars.addUint32("sintorn2.param.minZBits"               ) = c->getu32   ("minZBits"          ,9   ,"select number of Z bits - 0 mean max(xBits,yBits)"                               );
+  vars.addUint32("sintorn2.param.minZBits"               ) = c->getu32   ("minZBits"          ,10  ,"select number of Z bits - 0 mean max(xBits,yBits)"                               );
   vars.addUint32("sintorn2.param.tileX"                  ) = c->getu32   ("tileX"             ,8   ,"select tile X size"                                                              );
   vars.addUint32("sintorn2.param.tileY"                  ) = c->getu32   ("tileY"             ,8   ,"select tile Y size"                                                              );
   vars.addUint32("sintorn2.param.propagateWarps"         ) = c->getu32   ("propagateWarps"    ,4   ,"number of warps cooperating on propagating data in hierarchy (for AMD 4 is good)");
