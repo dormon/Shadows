4e6ee5778de86135dc91925cf02b16189308588d
Tomáš Milet <amillhouse@seznam.cz>
2020-06-08 01:26:25 +0200

    drawSilhouettes - edgePlanes

--- src/RSSV/debug/drawSilhouettes.cpp
+++ src/RSSV/debug/drawSilhouettes.cpp
@@ -17,9 +17,13 @@ namespace rssv::debug{
 void prepareDrawSilhouettes(vars::Vars&vars){
   FUNCTION_PROLOGUE("rssv.method.debug"
       ,"wavefrontSize"                        
+      ,"rssv.param.alignment"
+      ,"adjacency"
       );
 
-  auto const alignedNofEdges =  vars.getUint32  ("rssv.method.alignedNofEdges"  );
+  auto const alignSize                    =  vars.getSizeT       ("rssv.param.alignment"                   );
+  auto adj                                =  vars.get<Adjacency> ("adjacency"                              );
+  auto const nofEdges                     =  adj->getNofEdges();
 
   std::string const vsSrc = R".(
   #version 450
@@ -41,7 +45,7 @@ void prepareDrawSilhouettes(vars::Vars&vars){
 
   std::string const gsSrc = R".(
 
-layout(binding=0)readonly buffer EdgeBuffer       {float edgeBuffer       [];};
+layout(binding=0)readonly buffer EdgePlanes       {float edgePlanes       [];};
 layout(binding=1)readonly buffer MultBuffer       {
   uint  nofSilhouettes  ;
   uint  multBuffer    [];
@@ -56,6 +60,13 @@ flat in uint vId[];
 uniform mat4 view;
 uniform mat4 proj;
 
+#ifndef ALIGN_SIZE
+#define ALIGN_SIZE 128
+#endif//ALIGN_SIZE
+#define ALIGN(W,A) uint(uint(uint(W)/uint(A))*uint(A) + uint((uint(W)%uint(A))!=0u)*uint(A))
+#define ALIGN_SIZE_FLOAT ALIGN(ALIGN_SIZE,4u)
+#define ALIGN_OFFSET(i) uint(ALIGN(NOF_EDGES,ALIGN_SIZE_FLOAT)*uint(i))
+
 void main(){
   uint thread = vId[0];
 
@@ -66,13 +77,13 @@ void main(){
   int  mult = int(res) >> 29;
 
   vec4 P[2];
-  P[0][0] = edgeBuffer[edge+0*ALIGNED_NOF_EDGES];
-  P[0][1] = edgeBuffer[edge+1*ALIGNED_NOF_EDGES];
-  P[0][2] = edgeBuffer[edge+2*ALIGNED_NOF_EDGES];
+  P[0][0] = edgePlanes[edge+ALIGN_OFFSET(0)];
+  P[0][1] = edgePlanes[edge+ALIGN_OFFSET(1)];
+  P[0][2] = edgePlanes[edge+ALIGN_OFFSET(2)];
   P[0][3] = 1;
-  P[1][0] = edgeBuffer[edge+3*ALIGNED_NOF_EDGES];
-  P[1][1] = edgeBuffer[edge+4*ALIGNED_NOF_EDGES];
-  P[1][2] = edgeBuffer[edge+5*ALIGNED_NOF_EDGES];
+  P[1][0] = edgePlanes[edge+ALIGN_OFFSET(3)];
+  P[1][1] = edgePlanes[edge+ALIGN_OFFSET(4)];
+  P[1][2] = edgePlanes[edge+ALIGN_OFFSET(5)];
   P[1][3] = 1;
 
   gl_Position = proj*view*P[0];EmitVertex();
@@ -86,7 +97,8 @@ void main(){
   auto vs = make_shared<Shader>(GL_VERTEX_SHADER,vsSrc);
   auto gs = make_shared<Shader>(GL_GEOMETRY_SHADER,
       "#version 450\n",
-      Shader::define("ALIGNED_NOF_EDGES",alignedNofEdges),
+      Shader::define("ALIGN_SIZE",(uint32_t)alignSize),
+      Shader::define("NOF_EDGES" ,(uint32_t)nofEdges ),
       gsSrc);
   auto fs = make_shared<Shader>(GL_FRAGMENT_SHADER,
       "#version 450\n",
@@ -108,11 +120,11 @@ void drawSilhouettes(vars::Vars&vars){
   auto const adj               =  vars.get<Adjacency>  ("adjacency"                               );
   auto const vao               =  vars.get<VertexArray>("rssv.method.debug.vao"                   );
   auto const prg               =  vars.get<Program>    ("rssv.method.debug.drawSilhouettesProgram");
-  auto const edges             =  vars.get<Buffer>     ("rssv.method.edgeBuffer"                  );
+  auto const edgePlanes        =  vars.get<Buffer>     ("rssv.method.edgePlanes"                  );
   auto const multBuffer        =  vars.get<Buffer>     ("rssv.method.debug.dump.multBuffer"       );
   auto nofEdges = adj->getNofEdges();
 
-  edges->bindBase(GL_SHADER_STORAGE_BUFFER,0);
+  edgePlanes->bindBase(GL_SHADER_STORAGE_BUFFER,0);
   multBuffer->bindBase(GL_SHADER_STORAGE_BUFFER,1);
 
   vao->bind();
